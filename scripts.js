459a20f664be12df4ce5b035d8c5626dcb3104b7
HOPOLOK <vadim.truhanov@inbox.lv>
2025-01-03 15:19:12 +0200

    Some renaming in html and script getting all nessesary DOM elements

---
+++ scripts.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", (event) => {
+    const add_name = document.getElementById("add-name");
+    const add_info = document.getElementById("add-info");
+    const add_img = document.getElementById("add-img");
+    const add_category = document.getElementById("add-category");
+    const add_submit = document.getElementById("add-submit");
+
+    const search_input = document.getElementById("search-input");
+    const search_submit = document.getElementById("search-submit");
+
+    const filter_select = document.getElementById("filter-select");
+    const filter_remove = document.getElementById("filter-remove");
+    const filter_submit = document.getElementById("filter-submit");
+    const filter_container = document.getElementById("filter-conatiner");
+    const filter_checkboxes = {
+        "zombies" : document.getElementById("checkbox-zombies"),
+        "skills" : document.getElementById("checkbox-skills"),
+        "events" : document.getElementById("checkbox-events")
+    };
+
+    const cards_container = document.getElementById("cards-container");
+
+})
+
+
